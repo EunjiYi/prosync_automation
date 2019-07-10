aaeb3cc08b8d5c80202da63f7fecafe37e514f15
hsb3618 <hsb19@naver.com>
2019-07-10 14:59:51 +0900

    20190710

--- test.java
+++ test.java
@@ -13,52 +13,62 @@ public class test {
         
     Connection conn = null;
         try {
-//            String sql=args[0];
             // mysql 에서 tc id 추출 -> step 추출
             // 추출된 step 정제 후 배열에 저장
             // tc에 대한 precondition 케이스 소스/타겟 DB에 발생
             // 소스DB 부하
-            //ArrayList<TestCase()> list = new ArrayList<TestCase()>();
 
             // 추후 파일 읽어서 db정보 저장
-            DBConntion dbinfoTestlink = new DBConntion();
-            //DBConntion dbinfoSource = new DBConntion();
-            //DBConntion dbinfoTarget = new DBConntion();
-            dbinfoTestlink.setDbInfo("com.mysql.jdbc.Driver","jdbc:mysql://192.168.1.154:3307/bitnami_testlink","root","testlink"); 
-            //dbinfoSource.setDbInfo("com.mysql.jdbc.Driver","jdbc:mysql://192.168.1.154:3307/bitnami_testlink","root","testlink"); 
-            //dbinfoTarget.setDbInfo("com.mysql.jdbc.Driver","jdbc:mysql://192.168.1.154:3307/bitnami_testlink","root","testlink"); 
+            DBConntion dbinfo = new DBConntion();
+            dbinfo.setDbInfo("com.mysql.jdbc.Driver","jdbc:mysql://192.168.1.154:3307/bitnami_testlink","root","testlink"); 
+            conn = dbinfo.getConnection();
 
-            // mysql
             //conn = DBConntion.getConnection("com.mysql.jdbc.Driver","jdbc:mysql://192.168.1.154:3307/bitnami_testlink","root","testlink");
-            conn = dbinfoTestlink.getConnection();
-            // oracle
             //conn = DBConntion.getConnection("oracle.jdbc.driver.OracleDriver","jdbc:oracle:thin:@192.168.17.104:1521:orcl","tibero","tmax");
-            // tibero
             //conn = DBConntion.getConnection("com.tmax.tibero.jdbc.TbDriver","jdbc:tibero:thin:@192.168.17.104:38629:tbsync1","tibero","tmax");
 
             Statement stmtDD = conn.createStatement();
             StringBuilder sb = new StringBuilder();
-            String sql = sb.append("SELECT * FROM bitnami_testlink.nodes_hierarchy where parent_id =409849").toString();
-            ResultSet rs = stmtDD.executeQuery(sql);
+            sb.append("SELECT b.id, a.name, b.preconditions  FROM bitnami_testlink.nodes_hierarchy a,bitnami_testlink.tcversions b where parent_id=417990 and b.id=a.id+1");
+            //sb.append("SELECT b.id, a.name, b.preconditions  FROM bitnami_testlink.nodes_hierarchy a,bitnami_testlink.tcversions b where parent_id=409849 and b.id=a.id+1");
+            ResultSet rs = stmtDD.executeQuery(sb.toString());
 
 
             // TestCase타입의 ArrayList를 생성하겠다.
             ArrayList<TestCase> tc = new ArrayList<TestCase>();
             
 
-            // 쿼리이 있을 때 까지 loop
+            // 프로젝트의 testcase 내용을 조회
+            int cnt=0;
             while(rs.next()){
-                // 내용이 있으면 테스트케이스의 id + 제목의 내용을 ArrayList에 추가
-                tc.add(new TestCase(rs.getInt("id"), rs.getString("name")));
-            }
+                // 내용이 있으면 테스트케이스의 id + subject + preconditions 내용을 ArrayList에 추가
+                tc.add(new TestCase(rs.getInt("id"), rs.getString("name"), rs.getString("preconditions")));
 
-            //stmtDD.executeUpdate(sql);
-            //conn.commit();
+                sb.delete(0,sb.length());
+                sb.append("SELECT id FROM bitnami_testlink.nodes_hierarchy where parent_id="+rs.getInt("id"));
+
+                Statement stmtDD1 = conn.createStatement();
+                ResultSet rs1 = stmtDD1.executeQuery(sb.toString());
+
+                sb.delete(0,sb.length());
+                sb.append("SELECT actions, expected_results FROM bitnami_testlink.tcsteps where id in (");
+                while(rs1.next()){
+                    sb.append(rs1.getString("id") + ",");
+                }
+                sb.setCharAt(sb.length()-1,')');
+                System.out.println(sb.toString());
+                rs1 = stmtDD1.executeQuery(sb.toString());
+                while(rs1.next()){
+                    tc.get(cnt).setStep(rs1.getString("actions"),rs1.getString("expected_results"));
+                }
+                cnt++;
+            }
             
 
             // ArrayList로 생성된 tc의 정보들을 조회
             for(TestCase t : tc){
                 t.getTestCase();
+                t.getStep();
             }
 
         }
@@ -76,35 +86,42 @@ public class test {
     }
 }
 
-
 class TestCase
 {
     private int id;
-    private String subject;
-    private String precondition;
-    private String step[];
+    private String subject, precondition;
+    private ArrayList<String> action = new ArrayList<String>();
+    private ArrayList<String> expected_result = new ArrayList<String>();
 
-    public TestCase(int id) {
-        this.id = id;
-    }
-    public TestCase(int id, String subject) {
+    // 하나의 테이블에 케이스의 id,subject 정보가 있어서, 생성자에 정보를 입력 받을 수 있도록
+    public TestCase(int id, String subject, String precondition) {
         this.id = id;
         this.subject = subject;
+        this.precondition = precondition;
+    }
+    public void setStep(String action, String expected_result) {
+        this.action.add(action);
+        this.expected_result.add(expected_result);
     }
 
-    public void setTestCaseId(int id) {
-        this.id = id;
-    }
-
+    // 임시
     public void getTestCase() {
         System.out.println("tc id : " + this.id + ", subject : " + this.subject);
+        System.out.println("precondition : " + this.precondition);
     }
+    public void getStep() {
+        for(int i = 0; i < this.action.size(); i++) {
+            System.out.println(this.action.get(i));
+        }
+    }
+
+
 
 }
 
 class DBConntion
 {
-    String driver, url, user, pwd;
+    private String driver, url, user, pwd;
 
     public void setDbInfo(String driver, String url, String user, String pwd) {
         this.driver = driver;
