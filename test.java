eb3fd76edee841e25a471392e903af2b639e650d
Tmax <Tmax@DBQA-sangbok-heo>
2019-08-06 10:03:21 +0900

    check 로직 변경

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 import com.tmax.tibero.jdbc.driver.TbConnection;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class test {
 	public static String CommitUnit;
@@ -25,10 +26,13 @@ public class test {
 		ArrayList<TestCaseStep> tcStep = new ArrayList<TestCaseStep>();
 
 		Connection conn = null;
-		Connection connTar = null;
+
 		ResultSet rs = null;
 
 		try {
+			long start = System.currentTimeMillis();
+
+			Connection connTarget = null;
 			// 전반적인 과정
 			// 1) mysql(testlink) 에서 tc(testcase)의 id 추출 및 action 추출
 			// 2) 추출된 action을 정제 후(=HTML tag 제거 후) 배열로 저장
@@ -55,13 +59,13 @@ public class test {
 			// runpre라고 keyword 입력시 해당 구문 수행
 			if (args.length > 0 && args[0].equals("runpre")) {
 				conn = dbinfo.getConnection(1);
-				connTar = dbinfo.getConnection(2);
+				connTarget = dbinfo.getConnection(2);
 				for (TestCasePrecondition i : tcPre) {
 					sj.executePreQry(conn, i.getPrecondition());
-					sj.executePreQry(connTar, i.getPrecondition());
+					sj.executePreQry(connTarget, i.getPrecondition());
 				}
 				closeConnection(conn);
-				closeConnection(connTar);
+				closeConnection(connTarget);
 			}
 			// runaction이라고 keyword 입력시 해당 구문 수행
 			if (args.length > 0 && args[0].equals("runaction")) {
@@ -75,141 +79,123 @@ public class test {
 				closeConnection(conn);
 
 				ArrayList<String> syncTable = new ArrayList<String>();
-				Validation vd = new Validation();
 				int xid = 0, tsn = 0;
+				boolean colValidation = true, stepValidation = true, caseValidation = true;
 
 				conn = dbinfo.getConnection(1);
-				boolean suiteValidation = true;
+				int index = 0;
 				for (TestCaseStep i : tcStep) {
-					boolean caseValidation = true;
+					caseValidation = true;
 					for (String action : i.getAction()) {
 						// sync table parsing
-						String tmp1 = null;
+						String syncTableSplit = null;
 						if (action.contains("TBL")) {
-							System.out.println("SQL : " + action);
-							sj.executeActionQry(conn, action);
+							// System.out.println("SQL : " + action);
+							if (sj.executeActionQry(conn, action) < 0) {
+								System.out.println(
+										", CASE Name : " + tcPre.get(index).getSubject() + ", SQL : " + action);
+								continue;
+							}
 							rs = conn.createStatement().executeQuery(
 									"select dbms_transaction.local_transaction_id as tid, decode(dbms_transaction.local_transaction_id,null,null,current_tsn) as tsn from v$database;\n"
 											+ "");
-
-							tmp1 = action.substring(action.indexOf("TBL"));
-							tmp1 = tmp1.split(" ")[0];
-							tmp1 = tmp1.split("\\(")[0];
-							tmp1 = tmp1.split("\\,")[0];
-							tmp1 = tmp1.split(";")[0];
-
-							while (rs.next()) { // 내용이 있으면 테스트케이스의 id + subject + preconditions 내용을
+							while (rs.next()) {
 								String str = rs.getString("tid");
 								tsn = rs.getInt("tsn");
-
 								if (str != null && !str.isEmpty()) {
 									xid = Integer.parseInt(str.split("\\.")[0]) * 65536
 											+ Integer.parseInt(str.split("\\.")[1]);
-									System.out.println("XID : " + xid + ", TSN : " + tsn);
 								}
 							}
-							if (!syncTable.contains(tmp1)) {
-								syncTable.add(tmp1);
+							syncTableSplit = action.substring(action.indexOf("TBL"));
+							syncTableSplit = syncTableSplit.split(" ")[0].split("\\(")[0].split("\\,")[0].split(";")[0];
+
+							if (!syncTable.contains(syncTableSplit)) {
+								syncTable.add(syncTableSplit);
 							}
 						} else if (action.contains("ROLLBACK")) {
 							conn.rollback();
 							syncTable.clear();
 						} else {
 							// check logic
-							boolean stepValidation = true;
-							// commit 수행
+							if (syncTable.size() == 0) {
+								break;
+							}
+							stepValidation = true;
 							conn.commit();
-							connTar = dbinfo.getConnection(2);
-							boolean flag = true;
-							while (flag) {
-								rs = connTar
-										.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)
+							connTarget = dbinfo.getConnection(2);
+
+							// 프로싱크 동기화 여부 확인 (last tsn 조회)
+							do {
+								// System.out.println("SELECT TSN FROM prosync_t2t.prs_lct_t0 WHERE xid = " +
+								// xid + " AND tsn >= " + tsn);
+								rs = connTarget.createStatement()
 										.executeQuery("SELECT TSN FROM prosync_t2t.prs_lct_t0 WHERE xid = " + xid
 												+ " AND tsn >= " + tsn);
-								System.out.println("SELECT TSN FROM prosync_t2t.prs_lct_t0 WHERE xid = " + xid
-										+ " AND tsn >= " + tsn);
 								Thread.sleep(200);
-								while (rs.next()) {
-									if (rs.getString("TSN") != null && !rs.getString("TSN").isEmpty()) {
-										System.out.println("TSN : " + rs.getString("TSN"));
-										// 소스 /타겟 데이터 조회
+							} while (!rs.isBeforeFirst());
 
-										System.out.println(syncTable.get(0) + "멈춤 확인용");
-										for (String tbl : syncTable) {
-											System.out.println("동기화 체크할 테이블 : " + tbl);
-											ResultSet rs1 = conn
-													.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-															ResultSet.CONCUR_UPDATABLE)
-													.executeQuery("select * from " + tbl + " order by 1");
-											ResultSet rs2 = connTar
-													.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-															ResultSet.CONCUR_UPDATABLE)
-													.executeQuery("select * from " + tbl + " order by 1");
+							// 마지막에 추가된 테이블부터 조회 하도록 뒤집기
+							Collections.reverse(syncTable);
+							for (String tbl : syncTable) {
+								// 해당 테이블의 총 row count 추출
+								int srcRowCount = sj.getRowCount(conn, tbl),
+										tarRowCount = sj.getRowCount(connTarget, tbl);
 
-											int srcRowCount = 0, tarRowCount = 0;
-											rs1.last();
-											rs2.last();
-											srcRowCount = rs1.getRow();
-											tarRowCount = rs2.getRow();
-											rs1.beforeFirst();
-											rs2.beforeFirst();
-
-											System.out.printf(tbl + " srcRowCount : " + srcRowCount + ", tarRowCount : "
-													+ tarRowCount);
-											if (srcRowCount == tarRowCount) {
-												System.out.println(", ROW수 같음");
-
-												ResultSetMetaData metaInfo1 = rs1.getMetaData();
-												boolean colValidation = true;
-												while (rs1.next() && rs2.next()) {
-
-													for (int l = 1; l <= metaInfo1.getColumnCount(); l++) {
-														if (rs1.getString(l) == null
-																|| rs1.getString(l).equals("") == true) {
-															// 컬럼의 값이 NULL 일 때 아무런 작업 안함
-															continue;
-														} else {
-															// 컬럼의 값이 not NULL, 소스 타겟 데이터 비교
-															if (rs1.getString(l).equals(rs2.getString(l))) {
-																// 같으면 true, 다르면 false, 결과에 AND 연산자, 한번이라도 실패면 실패(false)
-																colValidation &= true;
-															} else {
-																colValidation &= false;
-																System.out.println(metaInfo1.getColumnName(l)
-																		+ "컬럼 데이터 불일치 SRC : " + rs1.getString(l)
-																		+ ", TAR : " + rs2.getString(l));
-																break;
-															}
-														}
-													}
-												}
-												stepValidation = colValidation;
-												flag = false;
-											}
-
-											else {
-												System.out.println(", ROW수 다름");
-												stepValidation = false;
-												flag = false;
-												System.exit(0);
+								if (srcRowCount != tarRowCount) {
+									stepValidation = false;
+								} else if (srcRowCount == 0) {
+									// System.out.println(" ROW 없음");
+									continue;
+								} else {
+									// System.out.println(", ROW수 같음");
+									ResultSet rs1 = conn.createStatement()
+											.executeQuery("select * from " + tbl + " order by 1");
+									ResultSet rs2 = connTarget.createStatement()
+											.executeQuery("select * from " + tbl + " order by 1");
+									// row별 데이터 비교
+									ResultSetMetaData metaInfo1 = rs1.getMetaData();
+									colValidation = true;
+									// 여러 row에 대해서 컬럼값 비교, 하나의 컬럼이라도 정합성이 다를 경우 수행되지 않음
+									while (rs1.next() && rs2.next() && colValidation) {
+										for (int l = 1; l <= metaInfo1.getColumnCount(); l++) {
+											if (rs1.getString(l) == null || rs1.getString(l).equals("") == true) {
+												continue;
+											} else if (rs1.getString(l).equals(rs2.getString(l))) {
+												colValidation &= true;
+											} else {
+												System.out.println(metaInfo1.getColumnName(l) + "컬럼 데이터 불일치 SRC : "
+														+ rs1.getString(l) + ", TAR : " + rs2.getString(l));
+												System.out.println(tcPre.get(index).getSubject() + ", TBL : " + tbl);
+												colValidation &= false;
+												break;
 											}
 										}
-										syncTable.clear();
-										stepValidation &= stepValidation;
-										System.out.println("stepValidation : " + stepValidation);
 									}
+									stepValidation &= colValidation;
+								}
+								if (!stepValidation) {
+									System.out.println("STEP 단위 정합성 불일치 ");
+									break;
 								}
 							}
-							closeConnection(connTar);
+							// System.out.println("stepValidation : " + stepValidation + ", colValidation :
+							// " + colValidation);
+							syncTable.clear();
+							closeConnection(connTarget);
 						}
 					}
+					caseValidation &= stepValidation;
+					if (!caseValidation) {
+						System.out.println("CASE 단위 정합성 불일치 ");
+					}
+					System.out.println(tcPre.get(index++).getSubject() + ", caseValidation : " + caseValidation);
 				}
 				closeConnection(conn);
 			}
-
-		} catch (
-
-		SQLException e) {
+			long end = System.currentTimeMillis();
+			System.out.println("실행 시간 : " + (end - start) / 1000.0 + "(초)");
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			closeConnection(conn);
@@ -294,6 +280,12 @@ class SqlJob {
 		return this.rs1;
 	}
 
+	public int getRowCount(Connection conn, String tbl) throws SQLException {
+		rs = conn.createStatement().executeQuery("select count(*) as count from " + tbl);
+		rs.next();
+		return rs.getInt("count");
+	}
+
 	// db에 접속하여 precondion을 수행한다.
 	public void executePreQry(Connection conn, String sql) throws SQLException {
 		String[] sqlSplit = sql.split(";");
@@ -303,12 +295,12 @@ class SqlJob {
 	}
 
 	// db 에 접속하여 action을 수행한다.
-	public void executeActionQry(Connection conn, String sql) {
+	public int executeActionQry(Connection conn, String sql) {
 		try {
-			conn.createStatement().execute(sql);
+			return conn.createStatement().executeUpdate(sql);
 		} catch (SQLException e) {
-			System.out.println(e.getErrorCode());
-			e.getStackTrace();
+			System.out.print("Error Code : " + e.getErrorCode());
+			return e.getErrorCode();
 		}
 	}
 
@@ -331,6 +323,10 @@ class TestCasePrecondition {
 		this.precondition = precondition;
 	}
 
+	public String getSubject() {
+		return this.subject;
+	}
+
 	public int getId() {
 		return this.id;
 	}
@@ -341,23 +337,15 @@ class TestCasePrecondition {
 
 	// testlink에서 바로 가져올 경우 HTML tag가 달린 상태로 쿼리가 받아짐. precondition의 HTML tag를 없애는 과정
 	public void modifyPrecondition() {
-		this.precondition = this.precondition.replaceAll("<p>", "");
-		this.precondition = this.precondition.replaceAll("</p>", "");
-		this.precondition = this.precondition.replaceAll("<br />", "");
-		this.precondition = this.precondition.replaceAll("(\r|\n|\r\n|\n\r)", "");
-		this.precondition = this.precondition.replaceAll("&#39;", "'");
-		this.precondition = this.precondition.replaceAll("&nbsp;", " ");
+		this.precondition = this.precondition.replaceAll("<p>", "").replaceAll("</p>", "").replaceAll("<br />", "")
+				.replaceAll("(\r|\n|\r\n|\n\r)", "").replaceAll("&#39;", "'").replaceAll("&nbsp;", " ");
 	}
 }
 
 //step 가져와서 정제
 class TestCaseStep {
-	public TestCaseStep() {
-	}
-
 	private ArrayList<String> action = new ArrayList<String>();
 	private ArrayList<String> expected_result = new ArrayList<String>();
-	private ArrayList<String> checkFlag = new ArrayList<String>();
 
 	public void setStep(String action, String expected_result) {
 		this.action.add(action);
@@ -374,14 +362,10 @@ class TestCaseStep {
 
 	// testlink에서 바로 가져올 경우 HTML tag가 달린 상태로 쿼리가 받아짐. action의 HTML tag를 없애는 과정
 	public void modifyAction(int index) {
-		this.action.set(index, this.action.get(index).replaceAll("<p>", ""));
-		this.action.set(index, this.action.get(index).replaceAll("</p>", ""));
-		this.action.set(index, this.action.get(index).replaceAll("<br />", ""));
-		this.action.set(index, this.action.get(index).replaceAll("(\r|\n|\r\n|\n\r)", ""));
-		this.action.set(index, this.action.get(index).replaceAll("&lt;", "<"));
-		this.action.set(index, this.action.get(index).replaceAll("&gt;", ">"));
-		this.action.set(index, this.action.get(index).replaceAll("&#39;", "'"));
-		this.action.set(index, this.action.get(index).replaceAll("&nbsp;", " "));
+		this.action.set(index,
+				this.action.get(index).replaceAll("<p>", "").replaceAll("</p>", "").replaceAll("<br />", "")
+						.replaceAll("(\r|\n|\r\n|\n\r)", "").replaceAll("&lt;", "<").replaceAll("&gt;", ">")
+						.replaceAll("&#39;", "'").replaceAll("&nbsp;", " "));
 	}
 }
 
